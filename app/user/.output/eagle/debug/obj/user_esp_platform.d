733c55e51ce8e24c1a4da8fc6478b741bb566153
baojun.fu <fubaojun2006@126.com>
2015-12-11 19:07:02 +0800

    change uart0 baudrate to 115200 for output error

---
+++ app/user/.output/eagle/debug/obj/user_esp_platform.d
@@ -0,0 +1,26 @@
+.output/eagle/debug/obj/user_esp_platform.o .output/eagle/debug/obj/user_esp_platform.d : user_esp_platform.c ../../include/ets_sys.h \
+ ../../include/c_types.h ../../include/eagle_soc.h \
+ ../../include/os_type.h ../../include/ets_sys.h ../../include/mem.h \
+ ../../include/osapi.h \
+ /opt/Espressif/crosstool-NG/xtensa-lx106-elf/xtensa-lx106-elf/include/string.h \
+ /opt/Espressif/crosstool-NG/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /opt/Espressif/crosstool-NG/xtensa-lx106-elf/xtensa-lx106-elf/include/newlib.h \
+ /opt/Espressif/crosstool-NG/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/config.h \
+ /opt/Espressif/crosstool-NG/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/ieeefp.h \
+ /opt/Espressif/crosstool-NG/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/features.h \
+ /opt/Espressif/crosstool-NG/xtensa-lx106-elf/xtensa-lx106-elf/include/xtensa/config/core-isa.h \
+ /opt/Espressif/crosstool-NG/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/reent.h \
+ /opt/Espressif/crosstool-NG/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /opt/Espressif/crosstool-NG/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/_types.h \
+ /opt/Espressif/crosstool-NG/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_types.h \
+ /opt/Espressif/crosstool-NG/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_default_types.h \
+ /opt/Espressif/crosstool-NG/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/lock.h \
+ /opt/Espressif/crosstool-NG/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ /opt/Espressif/crosstool-NG/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/cdefs.h \
+ /opt/Espressif/crosstool-NG/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/string.h \
+ ../include/user_config.h ../../include/user_interface.h \
+ ../../include/os_type.h ../../include/ip_addr.h ../../include/queue.h \
+ ../../include/spi_flash.h ../../include/espconn.h \
+ ../include/user_esp_platform.h ../include/user_iot_version.h \
+ ../include/user_config.h ../../include/upgrade.h ../include/user_light.h \
+ ../../include/pwm.h
---
+++ setenv_fuzi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# write this for config the ESP8266 development environment.
+# 2015-11-28 10:46:16
+#
+#PATH=~/Work/Espressif/crosstool-NG/builds/xtensa-lx106-elf/bin:$PATH
+PATH=/opt/Espressif/crosstool-NG/xtensa-lx106-elf/bin:$PATH
+echo $PATH
